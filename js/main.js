a30710ae1f6e63c0479bd62e074651b4879102f7
Joao Ruani <jvitornegri@gmail.com>
2018-12-09 11:25:36 -0200

    Increase search form and button

--- js/main.js
+++ js/main.js
@@ -2,17 +2,38 @@ var menuButton = document.getElementsByClassName('navbar-toggler'),
     icon = document.getElementById('menu-icon'),
     isClosed = true;
 
-
-menuButton[0].addEventListener('click', function(){
-    if(isClosed){
+// Icon change
+menuButton[0].addEventListener('click', function () {
+    if (isClosed) {
         icon.classList.remove('fa-bars');
         icon.classList.add('fa-times');
         isClosed = false;
     }
-    else{
+    else {
         icon.classList.remove('fa-times');
         icon.classList.add('fa-bars');
-        isClosed = true; 
+        isClosed = true;
     }
 });
+
+var text = document.getElementById("title"),
+    select = document.getElementById('user-type');
+    button = document.querySelectorAll('.register-button');
+    
+select.addEventListener('click', function(){
+    if(select.value == "candidato"){
+        text.textContent = "PROCURANDO A VAGA IDEAL?";
+        button.forEach(function(e){
+             e.textContent = 'CADASTRAR MEU CURRÍCULO!';
+        });
+    }
+    else{
+        text.textContent = "PROCURANDO O CANDIDATO IDEAL?";
+        button.forEach(function(e){
+            e.textContent = 'CADASTRAR MINHA EMPRESA!';
+        });
+    }
+});
+    
+
 
---
+++ js/search.js
@@ -0,0 +1,54 @@
+searchInput = document.getElementById('search');
+profissoes = ['Administrador', 'Advogado', 'Almoxarife', 'Analista de Qualidade', 'Analista de Riscos', 'Analista de Sistemas', 'Assessor', 'Assistente Administrativo', 'Auxiliar de enfermagem', 'Auxiliar de serviços gerais', 'Balconista', 'Barman', 'Caixa', 'Camareiro', 'Chefe de atendimento', 'Chefe de setor', 'Consultor', 'Contador', 'Coordenador', 'Cozinheiro', 'Designer']
+
+function autocomplete(inp, arr) {
+    var form = document.querySelector("form"),
+            recommendedSearch = document.createElement('ul');
+        recommendedSearch.classList.add('list-group');
+        recommendedSearch.classList.add('recommended-search');
+        recommendedSearch.classList.add('scrollSpy');
+        recommendedSearch.setAttribute('data-spy', 'scroll');
+        form.appendChild(recommendedSearch); 
+    closeAllLists(recommendedSearch);
+    
+    inp.addEventListener("input", function (e) {
+        var b, i, val = this.value;
+        closeAllLists(recommendedSearch);
+        if (!val) { 
+            recommendedSearch.classList.add('d-none');
+            return false;
+        }
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a LI element for each matching element:*/
+                b = document.createElement("li");
+                b.classList.add('list-group-item');
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (LI element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists(recommendedSearch);
+                });
+                recommendedSearch.appendChild(b);
+                recommendedSearch.classList.remove('d-none');
+            }
+        }
+    });
+    function closeAllLists(recommendedSearch) {
+        recommendedSearch.innerHTML = '';
+        recommendedSearch.classList.add('d-none');
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(recommendedSearch);
+    });
+}
+autocomplete(searchInput, profissoes);

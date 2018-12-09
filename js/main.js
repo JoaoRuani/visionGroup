57f0b5eb9603947ba5a70c4118e1935bcb8a38a7
Joao Ruani <jvitornegri@gmail.com>
2018-12-09 15:03:25 -0200

    screen erros fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var menuButton = document.getElementsByClassName('navbar-toggler'),
     icon = document.getElementById('menu-icon'),
     isClosed = true;
-
 // Icon change
 menuButton[0].addEventListener('click', function () {
     if (isClosed) {
@@ -18,22 +17,37 @@ menuButton[0].addEventListener('click', function () {
 
 var text = document.getElementById("title"),
     select = document.getElementById('user-type');
-    button = document.querySelectorAll('.register-button');
-    
-select.addEventListener('click', function(){
-    if(select.value == "candidato"){
+button = document.querySelectorAll('.register-button');
+
+select.addEventListener('click', function () {
+    if (select.value == "candidato") {
         text.textContent = "PROCURANDO A VAGA IDEAL?";
-        button.forEach(function(e){
-             e.textContent = 'CADASTRAR MEU CURRÍCULO!';
+        button.forEach(function (e) {
+            e.textContent = 'CADASTRAR MEU CURRÍCULO!';
         });
     }
-    else{
+    else {
         text.textContent = "PROCURANDO O CANDIDATO IDEAL?";
-        button.forEach(function(e){
+        button.forEach(function (e) {
             e.textContent = 'CADASTRAR MINHA EMPRESA!';
         });
     }
 });
-    
 
-
+width = window.innerWidth;
+searchInput = document.getElementById('search');
+if (width > 400){
+    select.classList.add('form-control-lg')
+    searchInput.classList.add('form-control-lg')
+}
+window.addEventListener('resize', function () {
+    width = window.innerWidth;
+    if (width > 400){
+        select.classList.add('form-control-lg');
+        searchInput.classList.add('form-control-lg');
+    }
+    else{
+        select.classList.remove('form-control-lg');
+        searchInput.classList.remove('form-control-lg');
+    }
+})

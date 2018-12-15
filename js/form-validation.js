e45aea6fb252acc096a002d8edbce00505cdabc7
Joao Ruani <jvitornegri@gmail.com>
2018-12-15 11:50:47 -0200

    New forms

--- js/form-validation.js
+++ js/form-validation.js
@@ -15,7 +15,7 @@ submitButton.addEventListener('click', function (event) {
 			if(!validaCNPJ(e.value)){
 				event.preventDefault();
 				e.classList.add('invalido');
-				error.textContent = 'Cnpj inválido';
+				error.textContent = 'CNPJ inválido';
 				errorCounter++;
 			}
 		}
@@ -32,6 +32,14 @@ submitButton.addEventListener('click', function (event) {
 				event.preventDefault();
 				e.classList.add('invalido');
 				error.textContent = 'A senha não atende aos requisitos';
+				errorCounter++;
+			}
+		}
+		else if(e.id == "cpf"){
+			if(!validaCPF(e.value)){
+				event.preventDefault();
+				e.classList.add('invalido');
+				error.textContent = 'CPF inválido';
 				errorCounter++;
 			}
 		}
@@ -61,13 +69,11 @@ function validaCNPJ(cnpj) {
 		//Verifica se não foram perdidos valores após a conversão, o que indica a presença de letras
 		if (CNPJ.length != 14) 
 			return false;
-		var pesos = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2],
-			resto = soma(pesos, CNPJ, 12) % 11;
-		if(!digitValidator(resto, CNPJ[12]))
+		var pesos = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+		if(!digitValidator(soma(pesos, CNPJ, 12), CNPJ[12]))
 			return false;
 		var pesos2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-		resto = soma(pesos2, CNPJ, 13);
-		if(!digitValidator(resto, CNPJ[13]))
+		if(!digitValidator(soma(pesos2, CNPJ, 13), CNPJ[13]))
 			return false;
 		return true;
 	}
@@ -98,4 +104,33 @@ function validaSenha(senha) {
 	if(letra && numero)
 		return true;
 	return false;
+}
+function validaCPF(cpf){
+	if (cpf.length != 11)
+		return false;
+	else {
+		var CPF = parseInt(cpf);
+		CPF = CPF.toString();
+		//Verifica se não foram perdidos valores após a conversão, o que indica a presença de letras
+		if (CPF.length != 11) 
+			return false;
+		if(!digitValidator(soma(CPF, 10, 9), CPF[9]))
+			return false;
+		if(!digitValidator(soma(CPF, 11,10), CPF[10]))
+			return false;
+		return true;
+	}
+	function soma(array1, start,end){
+		var soma = 0, cont = start;
+		for (let index = 0; index < end; index++, cont--) {
+			soma+= array1[index] * cont;	
+		}
+		return soma;
+	}
+	function digitValidator(resto, value){
+		var resultado = resto % 11 < 2 ? 0 : 11 - resto % 11;
+		if(resultado == value)
+			return true;
+		return false;
+	}
 }
